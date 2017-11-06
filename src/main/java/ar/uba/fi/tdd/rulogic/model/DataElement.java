e7e8c7bb83f626b4f4811a198fa196144703ba9f
alanbenju <alanbenju@hotmail.com>
2017-11-05 23:37:08 -0300

    working on DataElement

--- src/main/java/ar/uba/fi/tdd/rulogic/model/DataElement.java
+++ src/main/java/ar/uba/fi/tdd/rulogic/model/DataElement.java
@@ -1,15 +1,43 @@
 package ar.uba.fi.tdd.rulogic.model;
 
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+
 /**
  * Created by alan on 05/11/17.
  */
 public class DataElement {
     private String text;
     private String name;
+    private boolean isRule=false;
 
     public DataElement(String data){
-        this.text = data;
+        this.text = String.join("",data.split(" "));
         this.name = data.split("(")[0];
+
+        this.checkIfRule();
+    }
+
+    private void checkIfRule(){
+        String[] separated = this.text.split(":-");
+        this.isRule = separated.length > 1;
+    }
+
+    public boolean isRule() {
+        return this.isRule;
+    }
+
+    public ArrayList<DataElement> getFacts(){
+
+        return new ArrayList();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public boolean isEqual(DataElement query);
+        return true;
     }
 
 }
--- src/main/java/ar/uba/fi/tdd/rulogic/model/KnowledgeBase.java
+++ src/main/java/ar/uba/fi/tdd/rulogic/model/KnowledgeBase.java
@@ -1,5 +1,6 @@
 package ar.uba.fi.tdd.rulogic.model;
 
+import javax.xml.crypto.Data;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,20 +12,27 @@ import java.util.stream.Collectors;
 
 public class KnowledgeBase {
 	private Hashtable<String, ArrayList<DataElement>> facts;
-	private Hashtable<String, ArrayList<DataElement>> rules;
+	private Hashtable<String, ArrayList<DataElement>> rulesH;
     private ArrayList<DataElement> elements;
+    private ArrayList<DataElement> rules;
 
 	public KnowledgeBase(){
 		facts = new Hashtable();
-		rules = new Hashtable();
+		rulesH = new Hashtable();
+        rules = new ArrayList();
 		parseDB();
 	}
 
 	private void parseDB(){
 		String db = getBdFile("rules.db");
         String[] dbList = db.split("\\./n");
+        DataElement data;
 		for (int i = 0;i < dbList.length;i++){
-            elements.add(new DataElement(dbList[i]));
+            data = new DataElement(dbList[i]);
+            elements.add(data);
+            if (data.isRule()) rules.add(data);
+
+
 		}
 	}
 
@@ -39,9 +47,9 @@ public class KnowledgeBase {
 
 	public boolean answer(String query) {
 	    DataElement data = new DataElement(query);
-        DataElement[] factsToCheck = data.getFacts();
+        return true;
         /*
-        /*recorrer factsToCheck y chequear con la base de datos
+        /*recorrer lista y chequear con la base de datos
          */
         /*Iterator<String> nombreIterator = nombreArrayList.iterator();
         while(nombreIterator.hasNext()){
